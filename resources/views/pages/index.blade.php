b4f5a896b621ec0bba83bbfdb32ce2a225c438f9
vagrant <vagrant@homestead>
2017-04-03 08:32:49 +0000

    enlarge head

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -8,26 +8,26 @@
 
 @section('content')
 
-<div style="padding-left: -200px; padding-bottom:30px; word-break: none; word-wrap:break-word;" class="container col-md-10 col-xs-12">
+<div style="padding-left: -200px; padding-bottom:30px; word-break: none; word-wrap:none;" class="container col-md-10 col-xs-12">
   
-   	<div style="margin-top: 20px; padding-left: 0px"  class="col-md-12 col-xs-12" id="demo">
+    <div style="margin-top: 20px; padding-left: 0px"  class="col-md-12 col-xs-12" id="demo">
       <ul style="list-style: none; font-size: 20pt;">
-      	 <item  v-for="item in treeData"
+         <item  v-for="item in treeData"
           @if (Auth::guest())
             :admin="false"
           @else 
             :admin="true"
           @endif
-      	 	:key="item.name"
-  		    class="item"
-  		    :model="item">
-  		  </item>
+          :key="item.name"
+          class="item"
+          :model="item">
+        </item>
 
 
         @if (!Auth::guest())
-    		  <li class="">
-    		  	<input @keyup.enter="saveComment($event, 0)" type="text" class="form-control" placeholder="reply"/>
-    		  </li>
+          <li class="">
+            <input @keyup.enter="saveComment($event, 0)" type="text" class="form-control" placeholder="reply"/>
+          </li>
         @endif
       </ul>
     </div>
@@ -37,7 +37,7 @@
 
 
 
-    <div class="col-md-4 col-xs-4"  style="height: auto; position: fixed;  right:0px;
+    <div class="col-md-4 col-xs-4"  style="position: fixed;  right:0px;
    bottom:0px;" id="ModelContainer">
 
 </div>
